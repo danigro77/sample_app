1a05b40e8ef9650316a0dde93ae8d5f0857473ad
Daniela Grossmann <daniela.grossmann@gmail.com>
2012-05-26 14:19:59 -0700

    First Exercise passed

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -55,6 +55,23 @@ describe "Static pages" do
 
   end
 
+  describe "Contact page" do
+
+	it "should have the h1 'Contact'" do
+
+	    	visit '/static_pages/contact'	
+		page.should have_selector('h1', :text => 'Contact')
+
+	end
+
+	it "should have the title 'Contact'" do
+
+	    	visit '/static_pages/contact'	
+		page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact")
+
+	end
+
+  end
 
 #  describe "GET /static_pages" do
 #    it "works! (now write some real specs)" do
