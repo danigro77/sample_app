d491af4facf20e6684116e915b6ad13eb42d3517
Daniela Grossmann <daniela.grossmann@gmail.com>
2012-05-28 14:06:51 -0700

    Finsh user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+   factory :user do
+      name                      "Daniela Grossmann"
+      email                     "daniela.grossmann@gmail.com"
+      password                  "Ste11a2011"
+      password_confirmation     "Ste11a2011"
+   end
+end
