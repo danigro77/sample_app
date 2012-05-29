c79b790e3c8f367956bceab7975967c097a59033
Daniela Grossmann <daniela.grossmann@gmail.com>
2012-05-29 15:20:40 -0700

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
    factory :user do
-      name                      "Daniela Grossmann"
-      email                     "daniela.grossmann@gmail.com"
-      password                  "Ste11a2011"
-      password_confirmation     "Ste11a2011"
+      sequence(:name)            { |n| "Person #{n}" }                     
+      sequence(:email)           { |n| "person_#{n}@example.com" }                     
+      password                  "foobar"
+      password_confirmation     "foobar"
+
+      factory :admin do
+         admin true
+      end
    end
 end
